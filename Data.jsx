5f1170ef96ce178bf27e679797b29888576b8734
Amanku31 <146089497+Amanku31@users.noreply.github.com>
2024-01-22 11:49:34 +0530

    Create Data.jsx

---
+++ Data.jsx
@@ -0,0 +1,54 @@
+const Data =[
+    {
+        id: 1,
+        name:"Agra",
+        info:"Agra, a city in northern India, is primarily famous for the iconic Taj Mahal, one of the most recognized and celebrated landmarks in the world. The Taj Mahal is a white marble mausoleum built by the Mughal Emperor Shah Jahan in memory of his wife Mumtaz Mahal. It is renowned for its exquisite architecture, intricate carvings, and stunning symmetry.",
+        Image:"https://images.unsplash.com/photo-1616368309964-3a686de13b4d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"₹35,700",
+    },
+    {
+        id: 2,
+        name:"Jaipur",
+        info:"Jaipur is known for its rich history and is home to several historical monuments, including the Hawa Mahal, City Palace, Jantar Mantar, and the Amer Fort. These architectural marvels showcase the city's cultural and historical heritage.",
+        Image:"https://images.unsplash.com/photo-1603262110263-fb0112e7cc33?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"₹38,100",
+    },
+    {
+        id: 3,
+        name:"Ayodhya",
+        info:"Ayodhya is a city in the northern Indian state of Uttar Pradesh and is renowned for its historical, cultural, and religious significance.Ayodhya is considered the birthplace of Lord Rama, a central figure in the Hindu epic Ramayana. The city gained international attention due to the Ayodhya dispute, which revolved around the Babri Masjid located in the city. ",
+        Image:"https://www.jagranimages.com/images/newimg/09112023/09_11_2023-sdgg_31_23576554.webp",
+        price:"₹40,000",
+    },
+    {
+        id: 4,
+        name:"Banaras",
+        info:"Banaras, also known as Varanasi, is a city in the northern Indian state of Uttar Pradesh, Banaras is considered one of the holiest cities in Hinduism. It is situated on the banks of the River Ganges, and Hindus believe that taking a dip in the Ganges at Varanasi can cleanse them of their sins.",
+        Image:"https://images.unsplash.com/photo-1627894483216-2138af692e32?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"₹39,700",
+    },
+    {
+        id: 5,
+        name:"Goa",
+        info:"Goa is renowned for its beautiful and pristine beaches. Popular ones include Baga Beach, Calangute Beach, Anjuna Beach, and Palolem Beach, among others. These beaches attract tourists for sunbathing, water sports, and vibrant nightlife.",
+        Image:"https://images.unsplash.com/photo-1558960214-f4283a743867?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"₹45,500",
+    },
+    {
+        id: 6,
+        name:"Kashmir",
+        info:"Kashmir is renowned for its breathtaking landscapes, including the snow-capped Himalayan mountains, pristine lakes, lush green valleys, and meandering rivers. The region is often referred to as Paradise on Earth.",
+        Image:"https://images.unsplash.com/photo-1616190419596-e2839e7380d7?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"₹58,500",
+    },
+    {
+        id: 7,
+        name:"Kerela",
+        info:" Kerala is renowned for its picturesque backwaters, which consist of a network of lakes, rivers, and canals. Houseboat cruises through the backwaters, particularly in places like Alleppey and Kumarakom, are popular among tourists.",
+        Image:"https://images.unsplash.com/photo-1593682011146-7f6b1b055cde?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price:"₹60,690",
+    },
+];
+
+export default Data;
+
